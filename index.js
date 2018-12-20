a03c9f2332cfe9f6e1f8d43cbcab4521191360c5
macknight <rodrigopoloni@softbox.com.br>
2018-12-20 14:16:45 -0200

    objects

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
-
+//class creation
 var Person = function(name, age, job){
     this.name = name;
     this.age = age;
     this.job = job;
 }
-
+//adding method to prototype, this will be inhereted by all instances of person
+Person.prototype.smokeAJoint = function(amount){
+    console.log(this.name + ' smoked ' + amount +' grams of weed.')
+};
+//instantiating a new person called john 
 var john = new Person('john',15,'student');
 
 console.log(john.name);
 console.log(john.age);
 console.log(john.job);
+//makes john smoke two grams of marijuana
+john.smokeAJoint(2);
