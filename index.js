f491ec517c783ba3275942b8556d3d0dc37d3669
macknight <=rodrigo.macknight@gmail.com>
2018-12-20 16:26:21 -0200

    making the titlebar disapear

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //class creation
-var Person = function(name, age, job){
+const Person = function(name, age, job){
     this.name = name;
     this.age = age;
     this.job = job;
@@ -10,10 +10,35 @@ Person.prototype.smokeAJoint = function(amount){
     console.log(this.name + ' smoked ' + amount +' grams of weed.')
 };
 //instantiating a new person called john 
-var john = new Person('john',15,'student');
+const john = new Person('john',15,'student');
+john.job = 'dealer';
 
 console.log(john.name);
 console.log(john.age);
 console.log(john.job);
 //makes john smoke two grams of marijuana
 john.smokeAJoint(2);
+
+
+function showAllElements(){
+    var all = document.getElementsByTagName("*");
+
+    for (var i=0, max=all.length; i < max; i++) {
+        console.log(all[i]);
+    }
+}
+
+window.onload =function(){  
+
+    let titleHolder  = document.getElementById('huno');
+    console.log(titleHolder);
+    titleHolder.onclick = hideTitle;
+
+}
+
+
+
+function hideTitle(){
+    let titleHolder  = document.getElementById('huno');
+    titleHolder.style.display = 'none';
+};
